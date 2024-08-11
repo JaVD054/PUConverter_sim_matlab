7fb7eea61e9e380d5b21437c7320b78f2b235bd5
JaVD054 <jeedu124@gmail.com>
2024-08-11 14:06:13 +0530

    simple spwm implemented

--- switching.v
+++ switching.v
@@ -3,40 +3,34 @@ module switching(
     output reg t1, t2, t3, t4, t5, t6
 );
     
-    reg [5:0] switching_table [7:0];
 
 
     
-    initial begin
-        switching_table[0] = 6'b101010;
-        switching_table[1] = 6'b100011;
-        switching_table[2] = 6'b001110;
-        switching_table[3] = 6'b000111;
-        switching_table[4] = 6'b111000;
-        switching_table[5] = 6'b110001;
-        switching_table[6] = 6'b011100;
-        switching_table[7] = 6'b010101;
-    end
+        parameter state0 = 6'b101010,
+                state1 = 6'b100011,
+                state2 = 6'b001110,
+                state3 = 6'b000111,
+                state4 = 6'b111000,
+                state5 = 6'b110001,
+                state6 = 6'b011100,
+                state7 = 6'b010101;
     
-    reg [7:0] state_map [7:0];
     
-    initial begin
-            t1 = 0;t2 = 0;t3 = 0;t4 = 0;t5= 0;t6= 0;
-    end
 
     wire [7:0] state_mat;
     assign state_mat = {s1, s2, s3, s4, s5, s6, s7, s8};
     
     always @(*) begin
         case (state_mat)
-            8'b10000000: {t1, t2, t3, t4, t5, t6} = switching_table[0];
-            8'b01000000: {t1, t2, t3, t4, t5, t6} = switching_table[1];
-            8'b00100000: {t1, t2, t3, t4, t5, t6} = switching_table[2];
-            8'b00010000: {t1, t2, t3, t4, t5, t6} = switching_table[3];
-            8'b00001000: {t1, t2, t3, t4, t5, t6} = switching_table[4];
-            8'b00000100: {t1, t2, t3, t4, t5, t6} = switching_table[5];
-            8'b00000010: {t1, t2, t3, t4, t5, t6} = switching_table[6];
-            8'b00000001: {t1, t2, t3, t4, t5, t6} = switching_table[7];
+            8'b10000000: {t1, t2, t3, t4, t5, t6} = state0;
+            8'b01000000: {t1, t2, t3, t4, t5, t6} = state1;
+            8'b00100000: {t1, t2, t3, t4, t5, t6} = state2;
+            8'b00010000: {t1, t2, t3, t4, t5, t6} = state3;
+            8'b00001000: {t1, t2, t3, t4, t5, t6} = state4;
+            8'b00000100: {t1, t2, t3, t4, t5, t6} = state5;
+            8'b00000010: {t1, t2, t3, t4, t5, t6} = state6;
+            8'b00000001: {t1, t2, t3, t4, t5, t6} = state7;
+            default: {t1, t2, t3, t4, t5, t6} = 6'b000000;
         endcase
     end
 endmodule
